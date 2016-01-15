c5ced00b5a5299b60b42a7e70061945ab523c3c4
shawn <sxqwade@sina.com>
2016-01-15 19:03:51 +0800

    react-redux-webpack

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export App from './App/App';
+export Chat from './Chat/Chat';
+export Home from './Home/Home';
+export Login from './Login/Login';
+export LoginSuccess from './LoginSuccess/LoginSuccess';
+export NotFound from './NotFound/NotFound';
